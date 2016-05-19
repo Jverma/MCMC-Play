daa195180fc8610fdce6aa02a93b5455b7e4eb66
Irshad Mohammed <creativeishu@gmail.com>
2016-05-18 17:41:13 -0700

    data syntax changed

--- data/gen.py
+++ data/gen.py
@@ -11,7 +11,8 @@ def LinearData(X=np.linspace(100, 200, 25), m=1.0, c=0.0, RedStd=0.05):
 def WriteLinearFile(X=np.linspace(100, 200, 25), m=1.0, c=0.0, RedStd=0.05, plot=False):
 	filename = 'Linear_m%1.2f_c%1.2f_sd%1.2f_size%i.csv'%(m, c, RedStd, len(X))
 	data = LinearData(X, m, c, RedStd)
-	np.savetxt(filename, np.transpose(np.array(data)), delimiter=',', header='x, y, y_err')
+	np.savetxt(filename, np.transpose(np.array(data)), \
+				delimiter=',', header='x, y, y_err', fmt='%1.5f, %1.5f, %1.5f')
 	if plot:
 		plt.errorbar(data[0], data[1], data[2], color='k', ls='', ms=14)
 		plt.show()
@@ -24,7 +25,8 @@ def QuadraticData(X=np.linspace(100, 200, 25), a=1.0, b=1.0, c=0.0, RedStd=0.05)
 def WriteQuadraticFile(X=np.linspace(100, 200, 25), a=1.0, b=1.0, c=0.0, RedStd=0.05, plot=False):
 	filename = 'Quadratic_a%1.2f_b%1.2f_c%1.2f_sd%1.2f_size%i.csv'%(a, b, c, RedStd, len(X))
 	data = QuadraticData(X, a, b, c, RedStd)
-	np.savetxt(filename, np.transpose(np.array(data)), delimiter=',', header='x, y, y_err')
+	np.savetxt(filename, np.transpose(np.array(data)), \
+				delimiter=',', header='x, y, y_err', fmt='%1.5f, %1.5f, %1.5f')
 	if plot:
 		plt.errorbar(data[0], data[1], data[2], color='k', ls='', ms=14)
 		plt.show()
