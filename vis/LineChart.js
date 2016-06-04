a7de9a0f5e54973bcb6599c5e2d8c6f5233fa3f1
Jverma <j.verma5@gmail.com>
2016-06-04 18:21:19 -0400

    cleanup

--- vis/LineChart.js
+++ vis/LineChart.js
@@ -16,3 +16,23 @@ function drawLine(svg, data, m, c, x, y){
       .attr("class", "line")
       .attr("d", line);
 }
+
+
+
+function drawParabola(svg, data, a, b, c, x, y){
+	var new_data = data.map(function(d){
+		return {'x': d.x, 'y': a * Math.pow(d.x, 2) + b * d.x + c};
+	});
+
+	var line = d3.svg.line()
+	    .x(function(d) { return x(d.x); })
+	    .y(function(d) { return y(d.y); });
+
+	svg.select(".line")
+		.remove();
+
+  	svg.append("path")
+      .datum(new_data)
+      .attr("class", "line")
+      .attr("d", line);
+}
